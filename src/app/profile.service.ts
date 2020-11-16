ab45af7ab1d93cf4f3c14cfe6f021a4b2d337b8f
David5627 <binginaya708@gmail.com>
2020-11-16 15:02:26 +0300

    remove coment out

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -3,8 +3,7 @@ import {HttpClient} from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import{environment} from '../environments/environment'
-// import 'rxjs/add/operator/map';
-// import { resolveAny } from 'dns';
+
 
 @Injectable({
   providedIn: 'root'
